97e341fa23e209b412405e1db0d4c6094981ff82
Kunalsk36 <kunalkavathekar@gmail.com>
2025-07-14 00:30:20 +0530

    exercise 1-6 completed

--- ReactExcersice1-1/exe1/src/App.jsx
+++ ReactExcersice1-1/exe1/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const Header = ({ course }) => (
   <h1>{course.name}</h1>
 )
@@ -41,12 +43,29 @@ const App = () => {
       }
     ]
   }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
       <Header course={course} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
+
+      <h1>Give Feedback</h1>
+      <div>
+        <button onClick={() => setGood(good + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+      </div>
+      
+      <h2>Statistics</h2>
+      <div>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
     </div>
   )
 }
